1f708f00f7a42738c1692b4497ed548aa56baa00
Chris <96912309+Chris-Mwesigwa@users.noreply.github.com>
2023-08-24 00:00:55 +0300

    ini commit

---
+++ src/main/java/com/aplexy/cicdtest/CicdTestApplication.java
@@ -0,0 +1,14 @@
+package com.aplexy.cicdtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdTestApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CicdTestApplication.class, args);
+	}
+
+}
